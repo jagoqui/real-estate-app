37d68c051c33067dbc5c927f85a96a484bd6ba07
Jaidiver Gómez Quintero <jagoqui.gomez@gmail.com>
2025-09-25 17:40:11 -0500

    feat(shared/ky): add Ky HTTP client and basic configuration

---
+++ src/app/modules/shared/infrastructure/clients/ky/__test__/ky.client.test.ts
@@ -0,0 +1,22 @@
+import ky from 'ky'
+
+vi.mock('ky', () => {
+  const create = vi.fn()
+  return {
+    __esModule: true,
+    default: {create},
+    create,
+  }
+})
+
+const setup = async (): Promise<unknown> => await import('../ky.client')
+
+describe('API Configuration', () => {
+  it('should call ky.create with the correct configuration', async () => {
+    await setup()
+
+    expect(ky.create).toHaveBeenNthCalledWith(1, {
+      timeout: false,
+    })
+  })
+})
