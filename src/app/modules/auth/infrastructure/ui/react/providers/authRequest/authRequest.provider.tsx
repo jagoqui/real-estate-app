9474fb87161180e7b44027fd8a3c111bde106e75
Jaidiver Gómez Quintero <jagoqui.gomez@gmail.com>
2025-10-01 18:18:33 -0500

    Augment AuthRequestsProvider with navigation and initial refresh logic

--- src/app/modules/auth/infrastructure/ui/react/providers/authRequest/authRequest.provider.tsx
+++ src/app/modules/auth/infrastructure/ui/react/providers/authRequest/authRequest.provider.tsx
@@ -5,9 +5,11 @@ import {logoutRequest} from '@/modules/auth/infrastructure/requests/logout/logou
 import {refreshTokenRequest} from '@/modules/auth/infrastructure/requests/refreshToken/refreshToken.request';
 import {registerRequest} from '@/modules/auth/infrastructure/requests/register/register.request';
 import type {AuthResponse} from '@/modules/shared/domain/schemas/authResponse.schema';
+import {PATHNAME_ROUTES} from '@/modules/shared/infrastructure/ui/react/constants/main.constants';
 import {useAuthResponseContext} from '@/modules/shared/infrastructure/ui/react/contexts/authResponse/authResponse.context';
 import {googleLogout} from '@react-oauth/google';
-import {useMemo, type ReactNode} from 'react';
+import {useNavigate} from '@tanstack/react-router';
+import {useEffect, useMemo, useState, type ReactNode} from 'react';
 import {AuthRequestsContext} from '../../contexts/authRequests/authRequests.context';
 
 const AUTH_REQUESTS: AuthRequests = {
@@ -19,37 +21,67 @@ const AUTH_REQUESTS: AuthRequests = {
 };
 
 export const AuthRequestsProvider: React.FC<{children: ReactNode}> = ({children}) => {
-  const {setAuthResponse} = useAuthResponseContext();
+  const {authResponse, setAuthResponse} = useAuthResponseContext();
+  const navigate = useNavigate();
+
+  const [isFirstRender, setIsFirstRender] = useState(true);
+
+  useEffect(() => {
+    if (!isFirstRender) return;
+
+    if (authResponse?.refreshToken) {
+      const refresh = async (): Promise<void> => {
+        try {
+          const res = await refreshTokenRequest({refreshToken: authResponse.refreshToken});
+          setAuthResponse(res);
+        } catch (error) {
+          console.error('Error during token refresh on logout:', error);
+          setAuthResponse(null);
+        }
+      };
+      void refresh();
+      setIsFirstRender(false);
+    }
+  }, [authResponse, isFirstRender, setAuthResponse]);
 
   const wrappedRequests = useMemo<AuthRequests>(() => {
     return {
       registerRequest: async (args): Promise<AuthResponse> => {
         const res = await AUTH_REQUESTS.registerRequest(args);
         setAuthResponse(res);
+        await navigate({to: PATHNAME_ROUTES.HOME});
         return res;
       },
       loginWithEmailAndPasswordRequest: async (args): Promise<AuthResponse> => {
         const res = await AUTH_REQUESTS.loginWithEmailAndPasswordRequest(args);
         setAuthResponse(res);
+        await navigate({to: PATHNAME_ROUTES.HOME});
         return res;
       },
       loginWithGoogleRequest: async (args): Promise<AuthResponse> => {
         const res = await AUTH_REQUESTS.loginWithGoogleRequest(args);
         setAuthResponse(res);
+        await navigate({to: PATHNAME_ROUTES.HOME});
         return res;
       },
       refreshTokenRequest: async (args): Promise<AuthResponse> => {
         const res = await AUTH_REQUESTS.refreshTokenRequest(args);
         setAuthResponse(res);
+        await navigate({to: PATHNAME_ROUTES.HOME});
         return res;
       },
       logoutRequest: async (): Promise<void> => {
-        await AUTH_REQUESTS.logoutRequest();
+        const res = await AUTH_REQUESTS.logoutRequest();
+
         googleLogout();
+
         setAuthResponse(null);
+        await navigate({to: PATHNAME_ROUTES.LOGIN, replace: true});
+
+        return res;
       },
     };
-  }, [setAuthResponse]);
+  }, [setAuthResponse, navigate]);
 
   return (
     <AuthRequestsContext.Provider value={{...wrappedRequests}}>
