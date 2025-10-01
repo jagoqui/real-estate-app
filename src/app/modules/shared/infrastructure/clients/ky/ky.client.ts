fed0ce0cccddf6ed93a54662a2b76e0299b4cbdc
Jaidiver Gómez Quintero <jagoqui.gomez@gmail.com>
2025-10-01 15:32:48 -0500

    refactor: update ky client to inject Bearer token from shared domain logic

--- src/app/modules/shared/infrastructure/clients/ky/ky.client.ts
+++ src/app/modules/shared/infrastructure/clients/ky/ky.client.ts
@@ -1,5 +1,16 @@
+import {getAuthTokenBL} from '@/modules/shared/domain/businessLogic/getAuthTokenBL/getAuthTokenBL';
 import ky from 'ky';
 
 export const api = ky.create({
   timeout: false,
+  hooks: {
+    beforeRequest: [
+      (request: Request): void => {
+        const token = getAuthTokenBL();
+        if (token) {
+          request.headers.set('Authorization', `Bearer ${token}`);
+        }
+      },
+    ],
+  },
 });
