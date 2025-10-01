98148ab205666ea1e371b178a0b5d614f19b6efc
Jaidiver Gómez Quintero <jagoqui.gomez@gmail.com>
2025-10-01 15:32:47 -0500

    refactor(shared UI): migrate ShadCN UI components, constants, and utils to shared infrastructure

--- src/app/modules/shared/infrastructure/ui/shadcn/components/ui/button.tsx
+++ src/app/modules/shared/infrastructure/ui/shadcn/components/ui/button.tsx
@@ -2,7 +2,7 @@ import {Slot} from '@radix-ui/react-slot';
 import {type VariantProps} from 'class-variance-authority';
 import * as React from 'react';
 
-import {cn} from '@/lib/utils';
+import {cn} from '@/modules/shared/infrastructure/ui/shadcn/lib/utils';
 import {BUTTON_VARIANTS} from '../constants/ui.constants';
 
 function Button({
--- src/app/modules/shared/infrastructure/ui/shadcn/components/ui/separator.tsx
+++ src/app/modules/shared/infrastructure/ui/shadcn/components/ui/separator.tsx
@@ -2,7 +2,7 @@
 
 import * as SeparatorPrimitive from '@radix-ui/react-separator';
 
-import {cn} from '@/lib/utils';
+import {cn} from '@/modules/shared/infrastructure/ui/shadcn/lib/utils';
 
 function Separator({
   className,
