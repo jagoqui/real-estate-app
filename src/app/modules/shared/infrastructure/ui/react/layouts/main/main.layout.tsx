7a03144b3c85af644522078fdcbda4d208e50a2e
Jaidiver Gómez Quintero <jagoqui.gomez@gmail.com>
2025-10-05 20:27:38 -0500

    refactor: update main layout to use new user requests provider

--- src/app/modules/shared/infrastructure/ui/react/layouts/main/main.layout.tsx
+++ src/app/modules/shared/infrastructure/ui/react/layouts/main/main.layout.tsx
@@ -1,16 +1,19 @@
 import { queryClient } from '@/modules/shared/infrastructure/clients/query/query.client';
-import { AuthRequestsProvider } from '@/modules/shared/infrastructure/ui/react/providers/authRequest/authRequest.provider';
 import { QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { TanStackRouterDevtools } from 'node_modules/@tanstack/react-router-devtools/dist/esm/TanStackRouterDevtools';
+import { AuthRequestsProvider } from '../../providers/authRequests/authRequests.provider';
+import { UserRequestsProvider } from '../../providers/userRequests/userRequests.provider';
 
 export const MainLayout = ({ children }: { children: React.ReactNode }): React.ReactElement => (
   <>
     <AuthRequestsProvider>
-      <QueryClientProvider client={queryClient}>
-        {children}
-        <ReactQueryDevtools initialIsOpen={false} />
-      </QueryClientProvider>
+      <UserRequestsProvider>
+        <QueryClientProvider client={queryClient}>
+          {children}
+          <ReactQueryDevtools initialIsOpen={false} />
+        </QueryClientProvider>
+      </UserRequestsProvider>
     </AuthRequestsProvider>
     <TanStackRouterDevtools />
   </>
