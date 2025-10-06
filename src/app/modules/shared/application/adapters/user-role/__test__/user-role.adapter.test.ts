132867006e1b21fcfc94399d8d66b8373e232e1a
Jaidiver Gómez Quintero <jagoqui.gomez@gmail.com>
2025-10-05 20:27:37 -0500

    test: remove obsolete user-role.adapter test file

--- src/app/modules/shared/application/adapters/user-role/__test__/user-role.adapter.test.ts
+++
@@ -1,9 +0,0 @@
-import { ROLE_MAP, userRoleAdapter } from '../user-role.adapter';
-
-describe('userRoleAdapter', () => {
-  it('should map UserRoleDto to UserRole correctly', () => {
-    Object.entries(ROLE_MAP).forEach(([dto, role]) => {
-      expect(userRoleAdapter(Number(dto) as keyof typeof ROLE_MAP)).toBe(role);
-    });
-  });
-});
