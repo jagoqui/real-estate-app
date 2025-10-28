03231e443ffb69b8b4f62507532631d542a9b31b
Jaidiver Gómez Quintero <jagoqui.gomez@gmail.com>
2025-10-28 12:10:04 -0500

    chore: add recommended VSCode extensions and click-to-react-component dev tool

--- src/app/modules/shared/infrastructure/ui/react/layouts/main/main.layout.tsx
+++ src/app/modules/shared/infrastructure/ui/react/layouts/main/main.layout.tsx
@@ -1,6 +1,7 @@
 import { queryClient } from '@/modules/shared/infrastructure/clients/query/query.client';
 import { QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import { ClickToComponent } from 'click-to-react-component';
 import { TanStackRouterDevtools } from 'node_modules/@tanstack/react-router-devtools/dist/esm/TanStackRouterDevtools';
 import { AuthRequestsProvider } from '../../providers/authRequests/authRequests.provider';
 import { OwnersRequestsProvider } from '../../providers/ownersRequests/ownersRequests.provider';
@@ -19,5 +20,6 @@ export const MainLayout = ({ children }: { children: React.ReactNode }): React.R
       </UsersRequestsProvider>
     </AuthRequestsProvider>
     <TanStackRouterDevtools />
+    <ClickToComponent />
   </>
 );
