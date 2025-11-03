11c93b85fec39bd529faf8c02a2b9100573729ab
Jaidiver Gómez Quintero <jagoqui.gomez@gmail.com>
2025-11-03 18:31:11 -0500

    feat(vite): add react-scan plugin to Vite config for enhanced React development

--- config/vite/vite.config.react.ts
+++ config/vite/vite.config.react.ts
@@ -1,11 +1,20 @@
+import reactScan from '@react-scan/vite-plugin-react-scan';
 import react from '@vitejs/plugin-react';
-import {defineConfig, type UserConfig} from 'vite';
+import { defineConfig, type UserConfig } from 'vite';
 import viteConfigBase from './vite.config.base';
 
 export default defineConfig(
   configEnv =>
     ({
       ...viteConfigBase(configEnv),
-      plugins: [viteConfigBase(configEnv).plugins, react()],
-    }) satisfies UserConfig,
+      plugins: [
+        viteConfigBase(configEnv).plugins,
+        react(),
+        reactScan({
+          enable: true,
+          autoDisplayNames: true,
+          scanOptions: {},
+        }),
+      ],
+    }) satisfies UserConfig
 );
