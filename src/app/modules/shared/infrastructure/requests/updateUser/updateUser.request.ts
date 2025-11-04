90eb02eeee52569334376f032ce24d36af170598
Jaidiver Gómez Quintero <jagoqui.gomez@gmail.com>
2025-11-03 23:05:27 -0500

    refactor: update user update logic to use new dataToFormDataHelper and userDtoAdapter

--- src/app/modules/shared/infrastructure/requests/updateUser/updateUser.request.ts
+++ src/app/modules/shared/infrastructure/requests/updateUser/updateUser.request.ts
@@ -1,7 +1,8 @@
 import { userAdapter } from '@/modules/shared/application/adapters/user/user.adapter';
+import { userDtoAdapter } from '@/modules/shared/application/adapters/userDto/userDto.adapter';
 import type { UserDto } from '@/modules/shared/application/dtos/user.dto';
-import { createUserFormDataUseCase } from '@/modules/shared/application/useCases/createUserFormData/createUserFormData.uc';
 import type { UpdateUserRequest } from '@/modules/shared/domain/contracts/usersRequests.contract';
+import { dataToFormDataHelper } from '@/modules/shared/domain/helpers/dataToFormDataHelper/dataToFormDataHelper.helper';
 import { userSchema, type User } from '@/modules/shared/domain/schemas/user.schema';
 import { VARIABLES } from '@/variables/infrastructure/constants/variables.constants';
 import { api } from '../../clients/ky/ky.client';
@@ -9,7 +10,11 @@ import { api } from '../../clients/ky/ky.client';
 export const UPDATE_USER_REQUEST_URL = (userId: string): string => `${VARIABLES.VITE_API_BASE_URL}/users/${userId}`;
 
 export const updateUserRequest: UpdateUserRequest = async (args): Promise<User> => {
-  const body = createUserFormDataUseCase(args);
+  const userDto = userDtoAdapter(args.user);
+  const body = dataToFormDataHelper({
+    ...userDto,
+    photoFile: args.photoFile,
+  });
 
   const userResponseDto = await api.put<UserDto>(`${UPDATE_USER_REQUEST_URL(args.user.id)}`, { body }).json();
 
