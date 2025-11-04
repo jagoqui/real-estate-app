0c3bb8da68b3ae94e237ab5126a884989a12ad36
Jaidiver Gómez Quintero <jagoqui.gomez@gmail.com>
2025-11-03 23:05:27 -0500

    refactor: update user DTO adapter and usages to correct path

--- src/app/modules/shared/application/adapters/createUserDto/__test__/createUserDto.adapter.test.ts
+++ src/app/modules/shared/application/adapters/createUserDto/__test__/createUserDto.adapter.test.ts
@@ -1,8 +1,8 @@
 import { CREATE_USER_MOCK } from '@/data/mocks/createUser/createUser.mock';
 import { CREATE_USER_DTO_MOCK } from '@/data/mocks/createUser/createUserDto.mock';
 import { USER_DTO_MOCK } from '@/data/mocks/users/userDto.mock';
-import { createUserAdapter } from '../createUseDto.adapter';
-import * as userRoleDtoAdapterModule from './../../userRoleDto/userRoleDto.adapter';
+import * as userRoleDtoAdapterModule from '../../userRoleDto/userRoleDto.adapter';
+import { createUserAdapter } from '../createUserDto.adapter';
 
 describe('CreateUserDto Adapter', () => {
   beforeEach(() => {
