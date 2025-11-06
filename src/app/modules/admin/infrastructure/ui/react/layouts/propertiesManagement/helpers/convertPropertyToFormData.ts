55f305a78f6ee882dc2c48582b63e18db6dae790
Jaidiver Gómez Quintero <jagoqui.gomez@gmail.com>
2025-11-06 16:14:15 -0500

    Update helper to use update property form schema and action

--- src/app/modules/admin/infrastructure/ui/react/layouts/propertiesManagement/helpers/convertPropertyToFormData.ts
+++ src/app/modules/admin/infrastructure/ui/react/layouts/propertiesManagement/helpers/convertPropertyToFormData.ts
@@ -1,10 +1,13 @@
 import { type Property } from '@/modules/shared/domain/schemas/property.schema';
-import { propertyFormValuesSchema, type PropertyFormValues } from '@/modules/shared/domain/schemas/propertyForm.schema';
+import {
+  updatePropertyFormValuesSchema,
+  type PropertyFormValues,
+} from '@/modules/shared/domain/schemas/propertyForm.schema';
 
 export const convertPropertyToFormData = (property: Property): PropertyFormValues => {
   const formData = {
     ...property,
     action: 'update',
   };
-  return propertyFormValuesSchema.parse(formData);
+  return updatePropertyFormValuesSchema.parse(formData);
 };
