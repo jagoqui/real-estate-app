28f9322ef8c9e19e23b5e7d659341755ea114cf6
Jaidiver Gómez Quintero <jagoqui.gomez@gmail.com>
2025-11-07 10:52:14 -0500

    fix: Update return type of convertPropertyToFormData to ensure proper form value handling

--- src/app/modules/admin/infrastructure/ui/react/layouts/propertiesManagement/helpers/convertPropertyToFormData.ts
+++ src/app/modules/admin/infrastructure/ui/react/layouts/propertiesManagement/helpers/convertPropertyToFormData.ts
@@ -1,12 +1,13 @@
 import { type Property } from '@/modules/shared/domain/schemas/property.schema';
 import {
   updatePropertyFormValuesSchema,
-  type PropertyFormValues,
+  type UpdatePropertyFormValues,
 } from '@/modules/shared/domain/schemas/propertyForm.schema';
 
-export const convertPropertyToFormData = (property: Property): PropertyFormValues => {
-  const formData = {
+export const convertPropertyToFormData = (property: Property): UpdatePropertyFormValues => {
+  const formData: UpdatePropertyFormValues = {
     ...property,
+    imagesFiles: [],
     action: 'update',
   };
   return updatePropertyFormValuesSchema.parse(formData);
