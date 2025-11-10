da843891498e13dd6202e4d4a6470a08a32be5f4
Jaidiver Gómez Quintero <jagoqui.gomez@gmail.com>
2025-11-10 16:47:05 -0500

    refactor(requests): adapt getPropertiesByFilter.request to use PropertyFilters schema with GET and searchParams

--- src/app/modules/shared/infrastructure/requests/getPropertiesByFilter/getPropertiesByFilter.request.ts
+++ src/app/modules/shared/infrastructure/requests/getPropertiesByFilter/getPropertiesByFilter.request.ts
@@ -2,6 +2,7 @@ import { propertyAdapter } from '@/modules/shared/application/adapters/property/
 import type { PropertyResponseDto } from '@/modules/shared/application/dtos/propertyResponse.dto';
 import type { GetPropertiesByFilterRequest } from '@/modules/shared/domain/contracts/propertiesRequests.contract';
 import { propertySchema } from '@/modules/shared/domain/schemas/property.schema';
+import { propertyFiltersSchema } from '@/modules/shared/domain/schemas/propertyFilters.schema';
 import { VARIABLES } from '@/variables/infrastructure/constants/variables.constants';
 import { api } from '../../clients/ky/ky.client';
 
@@ -9,8 +10,8 @@ export const GET_PROPERTIES_BY_FILTER_REQUEST_URL = `${VARIABLES.VITE_API_BASE_U
 
 export const getPropertiesByFilterRequest: GetPropertiesByFilterRequest = async filter => {
   const propertyDto = await api
-    .post<Array<PropertyResponseDto>>(GET_PROPERTIES_BY_FILTER_REQUEST_URL, {
-      json: filter,
+    .get<Array<PropertyResponseDto>>(GET_PROPERTIES_BY_FILTER_REQUEST_URL, {
+      searchParams: propertyFiltersSchema.parse(filter),
     })
     .json();
 
